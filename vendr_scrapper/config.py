12720b661e8570024df1b7c680b01e909da071c8
Egorkud <egorkud2005@gmail.com>
2025-07-31 21:40:16 +0300

    chore(task1): add initial files + config

---
+++ vendr_scrapper/config.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+from fake_useragent import UserAgent
+
+load_dotenv()
+
+
+class Config:
+    # Database
+    DB_HOST = os.getenv('DB_HOST', 'localhost')
+    DB_PORT = int(os.getenv('DB_PORT', 5432))
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+    # Scraping
+    SCRAPE_CATEGORIES = [category.strip() for category in os.getenv('SCRAPE_CATEGORIES', '').split(',')]
+    MAX_WORKER_THREADS = int(os.getenv('MAX_WORKER_THREADS'))
+    USER_AGENT = UserAgent().random
+
+    # Logging
+    LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
