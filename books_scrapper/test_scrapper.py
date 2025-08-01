96a440256dbbaab9e34fa699b91bdff56ecc4868
Egorkud <egorkud2005@gmail.com>
2025-08-02 00:54:47 +0300

    feat(task2): add save to DB

--- books_scrapper/test_scrapper.py
+++ books_scrapper/test_scrapper.py
@@ -3,6 +3,7 @@ from contextlib import contextmanager
 from typing import Any
 
 from tqdm import tqdm
+from db.database import Database
 
 from config import Config
 from scrapper import BookScraper
@@ -45,11 +46,26 @@ if __name__ == "__main__":
         chunk_size = (len(urls) + num_workers - 1) // num_workers
         url_chunks = [urls[i:i + chunk_size] for i in range(0, len(urls), chunk_size)]
 
-        # TODO: add to DB
         # Запуск обробки даних кожної книжки
         with multiprocessing.Pool(processes=num_workers) as pool:
             results_nested = pool.map(scrape_worker, url_chunks)
 
         # Об'єднуємо списки результатів з кожного воркера
         results = [item for sublist in results_nested for item in sublist]
+
+        db = Database()
+        for book in results:
+            try:
+                db.insert_product(
+                    title=book.get("title", ""),
+                    category=book.get("category", ""),
+                    price=float(book.get("price", 0)),
+                    rating=book.get("rating", ""),
+                    stock_availability=book.get("stock_availability", ""),
+                    image_url=book.get("image_url", ""),
+                    description=book.get("description", ""),
+                    product_info=book.get("product_info", {})
+                )
+            except Exception as e:
+                print(f"Failed to insert book '{book.get('title')}' into database: {e}")
         print("All data saved")
